3e58ffab79ae1aecfe650210eee6c2620c58cae7
Sayantan Chakraborty <saychak2@in.ibm.com>
2018-03-25 11:17:19 +0530

    add files

---
+++ CustomUTI-Airdrop/AirDropData.swift
@@ -0,0 +1,9 @@
+//
+//  AirDropData.swift
+//  CustomUTI-Airdrop
+//
+//  Created by Sayantan Chakraborty on 25/03/18.
+//  Copyright © 2018 Sayantan Chakraborty. All rights reserved.
+//
+
+import Foundation
